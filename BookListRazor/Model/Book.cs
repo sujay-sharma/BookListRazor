3518c6a07eb36e6f74ef7c7ced7d38c6fb43e0a3
Sujay Sharma <sjsujay96@gmail.com>
2020-10-15 22:59:03 -0500

    Added Book page and form to create Book and update DB

---
+++ BookListRazor/Model/Book.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookListRazor.Model
+{
+    public class Book
+    {
+        [key] // Add ID automatically as an ID column
+        public int Id { get; set; }
+
+        [Required] // Name cannot be null
+        public string Name { get; set; }
+        public string Author { get; set; }
+
+        public string ISBN { get; set; }
+    }
+}
---
+++ BookListRazor/Model/keyAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BookListRazor.Model
+{
+    internal class keyAttribute : Attribute
+    {
+    }
+}
